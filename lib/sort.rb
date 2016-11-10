bdbcd90299d48af46fb8b7f68aa95d6bbaf5d00b
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-10 08:55:03 -0800

    switch

--- lib/sort.rb
+++ lib/sort.rb
@@ -6,4 +6,38 @@ class Sort
     @arr = arr
   end
 
+
+  def insert(array, right_index, value)
+    # value is the value of array[right_index + 1]
+    # right_index is the furthest right sorted element
+
+    # Step through sorted elements right to left.
+    # As long as your value is less than the element
+    # at array[i] and you still have elements
+    i = right_index
+    while(i >= 0 && array[i] > value)
+        # copy the element
+        array[i+1] = array[i]
+        i -= 1
+    end
+
+    # insert the actual element
+    array[i+1] = value;
 end
+
+  def insertion_sort
+    i = @arr.length
+
+    i.times do |j|
+      insert(@arr, i, @arr[0] )
+    end
+
+  end
+
+end
+
+#make position 0 the first pivot point
+
+
+#place everything that is less than the pivot point
+#to the left of pivot, greather than to the right
